5e76941fed6fc376519fd136436ed03ea95223ea
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-05-31 20:27:57 +0200

    Added function to copy transformations directly from animation source when it is inactive and to align the global transformation of the animation src if it is active and not mirroring

--- Scripts/RagDollPDController.cs
+++ Scripts/RagDollPDController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,33 +10,55 @@ using UnityEngine.Events;
 public class RagDollPDController : RagDollPDControllerBase
 {
 
+    [Serializable]
+    public class BodyMap{
+        public Transform src;
+        public Transform dst;
+    }
+
 
     List<Rigidbody> _mocapBodyParts;
     List<ArticulationBody> _mocapBodyPartsA;
     List<ArticulationBody> _bodyParts;
     RagDoll003 _ragDollSettings;
+    List<ArticulationBody> _bodies;
     List<ArticulationBody> _motors;
 
     public int numActionDims;
 
     public bool _hasLazyInitialized;
     public float[] _mocapTargets;
+    public bool activateExternalForce;
+    public  ArticulationBody root;
+    public float kp = 500f;//0.001f; // proportional gain
+    public float rkp = 5;//0.001f; // proportional gain
+    public float kd = 10f; // differential gain
+    public float limit = 10e14f;
+
+    public Vector3 rootForce;
+    public Vector3 rootTorque;
+    public Transform targetRoot;
+    public List<BodyMap> bodyMap;
+
+    public int solverIterations = 255;
     
+    public bool IsMirroring;
 
-    void Start(){
-    }
-
+     
     void FixedUpdate()
     {
-    
-        
         if (!_hasLazyInitialized)
         {
             OnEpisodeBegin();
             return;
         }
-        if(!active)return;
-        
+        if(!active){
+             foreach (var m in bodyMap){
+                m.dst.transform.position = m.src.position;
+                m.dst.transform.rotation = m.src.rotation;
+             }
+            return;
+        };
         var vectorAction = GetMocapTargets();
     
         int i = 0;
@@ -53,6 +76,39 @@ public class RagDollPDController : RagDollPDControllerBase
                 targetNormalizedRotation.z = vectorAction[i++];
             UpdateMotor(m, targetNormalizedRotation);
         }
+        if(activateExternalForce)ApplyExternalForceOnRoot();
+        if(!IsMirroring){
+            animationSrc.GetComponent<AnimStateController>().applyRootMotion = false;
+            animationSrc.GetComponent<Animator>().applyRootMotion = false;
+            SetReferenceRootTransform();
+        }
+       
+    }
+
+    public void ApplyExternalForceOnRoot(){
+        if(root == null) return;
+         var delta = (targetRoot.position - root.transform.position)*Time.fixedDeltaTime;
+         //delta.y = 0;
+         rootForce = kp * delta;
+         transform.position += delta;
+        var deltaV = (targetRoot.GetComponent<Rigidbody>().velocity - root.velocity);
+        rootForce += kd * deltaV;
+        root.AddForce(root.mass * rootForce);
+        rootForce = Vector3.ClampMagnitude(rootForce, limit);
+        var deltaQ = Quaternion.Inverse(root.transform.rotation)*targetRoot.rotation;
+        var deltaY = deltaQ.eulerAngles.y*Mathf.Deg2Rad;
+        rootTorque = rkp * Vector3.up*deltaY*Time.fixedDeltaTime/2;
+        //float angle;
+        //deltaQ.ToAngleAxis( out angle, out rootTorque);
+        rootTorque = Vector3.ClampMagnitude(rootTorque, limit);
+        root.AddTorque(root.mass * rootTorque);
+        foreach (var m in _motors)
+        {
+            m.AddForce(m.mass*rootForce);
+            m.AddTorque(m.mass*rootTorque);
+        }
+        //animationSrc.stabilizer.GetComponent<Rigidbody>().AddForce(rootForce);
+
     }
 
 
@@ -68,41 +124,38 @@ public class RagDollPDController : RagDollPDControllerBase
             _bodyParts = GetComponentsInChildren<ArticulationBody>().ToList();
             _ragDollSettings = GetComponent<RagDoll003>();
 
-            foreach (var body in GetComponentsInChildren<ArticulationBody>())
-            {
-                body.solverIterations = 255;
-                body.solverVelocityIterations = 255;
-            }
+            root = GetComponentsInChildren<ArticulationBody>().Where(x => x.isRoot).FirstOrDefault();
 
-            _motors = GetComponentsInChildren<ArticulationBody>()
+             if (_mocapBodyParts.Count > 0) {
+                Rigidbody mocapBody = _mocapBodyParts.First(x => x.name == animationSrc.rootName);
+                targetRoot = mocapBody.transform;
+            }
+            else
+            {
+                ArticulationBody mocapBody = _mocapBodyPartsA.First(x => x.name ==animationSrc.rootName);
+                targetRoot = mocapBody.transform;
+            }
+            _bodies =  GetComponentsInChildren<ArticulationBody>().ToList();
+
+            bodyMap = new List<BodyMap>();
+            bodyMap.Add(new BodyMap(){dst= root.transform, src = targetRoot.transform});
+            foreach (var b in _bodies){
+                var src = animationSrc.GetComponentsInChildren<Transform>().First(x => x.name == b.name);
+                bodyMap.Add(new BodyMap(){dst= b.transform, src = src});
+            }
+            _motors = _bodies
                 .Where(x => x.jointType == ArticulationJointType.SphericalJoint)
                 .Where(x => !x.isRoot)
                 .Distinct()
                 .ToList();
             var individualMotors = new List<float>();
             numActionDims = 0;
-            foreach (var m in _motors)
-            {
-                if (m.twistLock == ArticulationDofLock.LimitedMotion) { 
-                    individualMotors.Add(0f);
-                    numActionDims++;
-                }
-                if (m.swingYLock == ArticulationDofLock.LimitedMotion) { 
-                    individualMotors.Add(0f);
-                    numActionDims++;
-                }
-                if (m.swingZLock == ArticulationDofLock.LimitedMotion) { 
-                    individualMotors.Add(0f);
-                    numActionDims++;
-                }
-            }
+           
             _mocapTargets = null;
             _hasLazyInitialized = true;
             animationSrc.ResetToIdle();
             animationSrc.CopyStatesTo(this.gameObject, false);
         }
-
-     
         OnReset?.Invoke();
 
 #if UNITY_EDITOR
@@ -265,19 +318,48 @@ public class RagDollPDController : RagDollPDControllerBase
         }
     }
 
+    void DeactivateBody(ArticulationBody body)
+    {
+       body.enabled = false;
+         //joint.Sleep();
+    }
+
+    void ActivateBody(ArticulationBody body)
+    {
+        body.enabled = true;
+        
+        body.solverIterations = solverIterations;
+        body.solverVelocityIterations = solverIterations;
+        //joint.WakeUp();
+    }
+
     public void Deactivate(){
         active = false;
-        foreach (var m in _motors)
+        foreach (var m in _bodies)
         {
-            if (m.isRoot)
-                continue;
-            Vector3 targetNormalizedRotation = Vector3.zero;
-            DeactivateMotor(m);
+          DeactivateBody(m);
         }
+        if(!IsMirroring) SetReferenceRootTransform();
     }
 
      public void Activate(){
         active = true;
+        foreach (var m in _bodies)
+        {
+            ActivateBody(m);
+        }
+         if(!IsMirroring)animationSrc.CopyStatesTo(this.gameObject, false);
+    }
+
+    void SetReferenceRootTransform(){
+        var delta =root.transform.position - targetRoot.position;
+        delta.y = 0;
+        var deltaQ = Quaternion.Inverse(targetRoot.rotation)*root.transform.rotation;
+        var deltaY = deltaQ.eulerAngles.y;
+        deltaQ = Quaternion.AngleAxis(deltaY, Vector3.up);
+        animationSrc.transform.position += delta;
+        animationSrc.transform.rotation *= deltaQ;
+        
     }
 
 
